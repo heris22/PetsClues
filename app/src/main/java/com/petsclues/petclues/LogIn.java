37749128a8859afaee67038bbf41f30e4e2ab595
Hernán Quinde <hquinde@espol.edu.ec>
2019-02-01 15:40:04 -0500

    Implementación de firebase

--- app/src/main/java/com/petsclues/petclues/LogIn.java
+++ app/src/main/java/com/petsclues/petclues/LogIn.java
@@ -1,13 +1,91 @@
 package com.petsclues.petclues;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class LogIn extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private static final String TAG = "Registro";
+    private Button btnRegistro;
+    private EditText editContra, editEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        btnRegistro = (Button) findViewById(R.id.btnRegistro);
+        editEmail = (EditText) findViewById(R.id.txtEmail);
+        editContra = (EditText)  findViewById(R.id.txtContra);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if(firebaseUser != null){
+                    Log.w(TAG, "Usurio Logeado: " + firebaseUser.getEmail());
+                }else {
+                    Log.w(TAG, "Usurio NO Logeado");
+                }
+            }
+        };
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                crearCuenta();
+            }
+        });
+
     }
+
+    public void crearCuenta(){
+        String correo = editEmail.getText().toString();
+        String clave = editContra.getText().toString();
+        firebaseAuth.createUserWithEmailAndPassword(correo,clave).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LogIn.this, "cuenta creada", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(LogIn.this, "No se creo la cuenta", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+
 }
